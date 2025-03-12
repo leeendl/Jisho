959f81b86b119528154094c5f9426da4e52409df
LeeEndl <gavinolson71@gmail.com>
2025-03-12 18:26:45 -0500

    update gradle-wrapper to 8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.jisho"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.0-20"
+        versionCode = 21
+        versionName = "1.0-21"
 
         vectorDrawables {
             useSupportLibrary = true
