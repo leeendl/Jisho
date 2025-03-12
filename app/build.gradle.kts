ff905b01e8b6bcde46958b8c2b88b921fd9720b1
LeeEndl <gavinolson71@gmail.com>
2025-03-12 18:24:47 -0500

    update composeBom to 2025.03.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.jisho"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0-19"
+        versionCode = 20
+        versionName = "1.0-20"
 
         vectorDrawables {
             useSupportLibrary = true
