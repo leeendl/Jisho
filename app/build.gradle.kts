4cb2efae206b8514a87252f29b5e04a2b8cb09b2
LeeEndl <gavinolson71@gmail.com>
2025-05-19 19:38:35 -0500

    using commit SHA in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 21
         targetSdk = 36
 
-        versionCode = 56
-        versionName = "1.0-56"
+        versionCode = 57
+        versionName = "1.0-57"
 
     }
 
