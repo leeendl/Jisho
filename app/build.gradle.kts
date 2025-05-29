3da343a902cbee2d41c1480535259fee794e714f
leeendl <127639272+leeendl@users.noreply.github.com>
2025-05-29 16:28:51 -0500

    upgrade AGP to 8.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 21
         targetSdk = 36
 
-        versionCode = 63
-        versionName = "1.0-63"
+        versionCode = 64
+        versionName = "1.0-64"
 
     }
 
